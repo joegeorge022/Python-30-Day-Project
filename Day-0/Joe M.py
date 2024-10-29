200029ad7586e2e87e7fcbf554f679b4dde42b5f
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-10-29 15:16:52 +0530

    Add files via upload

---
+++ Day-0/Joe M.py
@@ -0,0 +1,9 @@
+"""
+AUTHOR : JOE MARTIN RINCE
+DATE: 28-10-2024
+PYTHON PROGRAM TO PRINT TEXT
+"""
+
+print("Hello World ")
+print("I am Joe Martin Rince")
+print("I love python ")
---
+++ Day-0/joe_g.py
@@ -0,0 +1,9 @@
+'''
+Python Program to print text
+Author = Joe George
+Date = October 28, 2024
+'''
+
+print("Hello World")
+
+print("I am Joe George.")
