8ec17439a3637c8c4a26b0b104bd59f02f317154
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-10-29 15:15:54 +0530

    Delete Day 0  directory

--- Day 0 /Joe M.py
+++
@@ -1,9 +0,0 @@
-"""
-AUTHOR : JOE MARTIN RINCE
-DATE: 28-10-2024
-PYTHON PROGRAM TO PRINT TEXT
-"""
-
-print("Hello World ")
-print("I am Joe Martin Rince")
-print("I love python ")
--- Day 0 /joe_g.py
+++
@@ -1,9 +0,0 @@
-'''
-Python Program to print text
-Author = Joe George
-Date = October 28, 2024
-'''
-
-print("Hello World")
-
-print("I am Joe George.")
