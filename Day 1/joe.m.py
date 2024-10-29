df9fa0cf3fe635be786cdaed8a90c48cb80b2a4e
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2024-10-29 15:09:05 +0530

    Add files via upload

---
+++ Day 1/joe.m.py
@@ -0,0 +1,14 @@
+"""
+AUTHOR : Joe Martin Rince
+DATE :29-10-2024
+"""
+full_name="Joe Martin Rince"
+age="18"
+is_student=True
+print(f"My name is {full_name} and I am {age} years old.")
+print(f"Are you a student? "
+      f"ans : {is_student}")
+if is_student:
+    print("I am a student")
+else:
+    print("I am not a student")
