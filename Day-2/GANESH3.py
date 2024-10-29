7b66a79a75a8401ed00533f02cc9e1f85f313092
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2024-10-29 15:51:32 +0530

    Add files via upload

---
+++ Day-2/GANESH3.py
@@ -0,0 +1,21 @@
+
+a = 11
+b = 52
+
+
+sum_result = a + b
+difference_result = a - b
+product_result = a * b
+division_result = a / b
+exponent_result = a ** b
+modulus_result = a % b
+floor_division_result = a // b
+
+
+print("Sum:", sum_result)
+print("Difference:", difference_result)
+print("Product:", product_result)
+print("Division:", division_result)
+print("Exponent:", exponent_result)
+print("Modulus:", modulus_result)
+print("Floor Division:", floor_division_result)
