d553a1c1541cc64eaf70387845f1c99a4f91ba73
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2024-10-29 15:31:31 +0530

    Add files via upload

---
+++ Day-2/joe.m.py
@@ -0,0 +1,18 @@
+"""AUTHOR : Joe Martin Rince
+DATE: 29-10-2024
+"""
+a=10
+b=20
+c=30
+sum_result=a+b+c
+difference_result=a-b
+product_result=a*b*c
+exponent_result=a**b
+modulus_result=a%b
+floor_result=a//b
+print(f"SUM: {sum_result}")
+print(f"DIFFERENCE: {difference_result}")
+print(f"PRODUCT:{product_result}")
+print(f"EXPONENT: {exponent_result}")
+print(f"MODULUS:  {modulus_result}")
+print(f"FLOOR RESULT: {floor_result}")
