89b70003905b378289489f8c503224abe8d4e856
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-10-30 18:28:30 +0530

    Create joe_g.py

---
+++ Day-2/joe_g.py
@@ -0,0 +1,49 @@
+# 3.ARITHMETIC
+
+#Addition
+friends = 5              #now printing friends =5
+
+friends +=5              #now printing friends= 5+5 =10
+                         #another way is (friends = friends + 5)
+print(friends)
+
+
+#Subtraction
+enemies = 10
+
+enemies -= 5             #now printing enemies= 10-5 =5
+                         #another way is (enemies = enemies - 5)
+print(enemies)
+
+
+#Multiplication
+allies = 10
+
+allies *= 2              #now printing allies= 5*2 =10
+                         #another way is (allies = allies*2)
+print(allies)
+
+
+#Division
+foes = 10
+
+foes /= 2                #now printing foes= 10/2 =5
+                         #another way is (foes = foes / 2)
+print(foes)
+
+
+#Integer Division
+# (Same as division, but it rounds off to the nearest integer)
+rivals = 10
+
+rivals //= 3             #now printing rivals =10/3 ≈3
+                         #another way is (rivals = rivals//3)
+print(rivals)
+
+
+#Remainder
+guys = 10
+
+guys %= 3                #now printing guys = 10/3 remainder=1
+                         #another way is (guys = guys%3)
+print(guys)
