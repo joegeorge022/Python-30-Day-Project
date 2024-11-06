0422b4ec71728d58024db52f9a97b0b22307235e
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-11-06 19:18:55 +0530

    Update job2.py

--- Day-1/job2.py
+++ Day-1/job2.py
@@ -1,12 +1,16 @@
 '''Author: Job Thomas Cherian
    Day 1
    program for name and age'''
+
 Name=input("Enter your name:")
 Age=int(input("Enter your age:"))
+
 is_student=True
+
 print(f"Hello {Name}")
 print(f"hey {Name} you are {Age} old now")
 print(f"are you a student? ans:{is_student}")
+
 if is_student:
     print("he is a student")
 else:
