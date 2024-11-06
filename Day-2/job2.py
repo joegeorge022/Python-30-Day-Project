eeeb5933a770d836866d21a8baf895ebb1b61d2d
Jobthomas10 <jobthomascherian2028@cs.sjcetpalai.ac.in>
2024-11-06 07:45:50 -0800

    Add files via upload

---
+++ Day-2/job2.py
@@ -0,0 +1,19 @@
+'''
+Author: Job Thomas Cherian
+06/11/24
+program for arithmetic operators
+'''
+a=int(input("Enter a number:"))
+b=int(input("Enter a number"))
+sum = a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+floor =a//b
+exponentional=a**b
+print(f"sum:{sum}")
+print(f"difference:{subtraction}")
+print(f"multiplication:{multiplication}")
+print(f"divison:{division}")
+print(f"floor:{floor}")
+print(f"exponentional:{exponentional}")
