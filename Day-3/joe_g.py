1ca3768de7d96476e5a3650b2869110667a41180
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-11-07 20:25:50 +0530

    Create joe_g.py

---
+++ Day-3/joe_g.py
@@ -0,0 +1,11 @@
+'''
+Author: Joe George
+Date: November 7, 2024
+'''
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+age = int(age)
+
+print("Hello, ",name,"! You are ",age," years old.")
