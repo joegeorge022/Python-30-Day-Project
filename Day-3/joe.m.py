cae127e1aa09eaa8906f512d84dea7b1d565c1cb
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2024-11-08 07:04:20 +0530

    Add files via upload

---
+++ Day-3/joe.m.py
@@ -0,0 +1,11 @@
+"""
+AUTHOR : JOE MARTIN RINCE
+DATE ; 7-11-2024
+"""
+input("Enter your name : ")
+
+int(input("Enter a number: "))
+
+float(input("Enter a decimal number : "))
+
+str(input("Do you love python ? : "))
