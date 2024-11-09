2eaf3a3516082030ff920436f51b66e621111b29
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2024-11-09 16:37:06 +0530

    Add files via upload

---
+++ Day-4/joe.m.py
@@ -0,0 +1,20 @@
+"""
+AUHTOR ; JOE MARTIN RINCE
+DATE : 8-11-2024
+"""
+greetings ="Hello"
+name ="SAM"
+
+full_greetings=greetings + "," + name + "!" +"  " + "Nice to meet you"
+print(full_greetings)
+
+message = "HI I AM SAM"
+print(message[0:2])
+
+sentence = "Python is amazing"
+print(sentence.upper())
+print(sentence.split())
+print(sentence.replace("amazing","great"))
+
+quote= "He said ,\"python is amazing.\""
+print(quote)
