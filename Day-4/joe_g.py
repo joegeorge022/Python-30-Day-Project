970231c18819655164a440ae8a7e504d645f6f1a
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-11-09 22:37:33 +0530

    Add files via upload

---
+++ Day-4/joe_g.py
@@ -0,0 +1,28 @@
+'''
+Author = Joe George
+Date = November 9, 2024
+'''
+
+#Assigning Strings
+f_name="Joe"
+l_name="George"
+status="Alive"
+
+#Concatenation
+name = f_name+" "+l_name
+
+
+print(f"My name is {name} and I am {status}.")
+
+#String Slicing
+print(name[0:3])  #Separates first name from full name.
+
+#Fomatting
+sentence = "What am I even doing ?"
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.split())
+print(sentence.replace("?","with life?"))
+
+
+print("Miss Mary said, \"I like Poppins.\"")
