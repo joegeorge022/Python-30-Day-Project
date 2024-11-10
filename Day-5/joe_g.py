2c6fdf53e5c098cb50693a5202e360b77ecfb840
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-11-10 21:31:10 +0530

    Update joe_g.py

--- Day-5/joe_g.py
+++ Day-5/joe_g.py
@@ -7,6 +7,7 @@ def function():                 #Here we defined a function using def function()
     print("Hello World")        #The function to be executed is print("Hello World")
 
 
-function()                      #Now typing just function() will perform the print function
+function()                      #Now typing just function() will perform the function that we assigned earlier.
+                                #In this case  function() will print Hello World
 
 
