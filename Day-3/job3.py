b6ff00abc2737f3b4786fb9713005979c2507311
Jobthomas10 <jobthomascherian2028@cs.sjcetpalai.ac.in>
2024-11-11 05:41:40 -0800

    Add files via upload

---
+++ Day-3/job3.py
@@ -0,0 +1,15 @@
+'''
+Author: Job Thomas Cherian
+06/11/24
+program using input function
+'''
+name=input("Enter your name:")
+age=int(input("enter your age:"))
+age=str(age)
+print(f"Hello {name}. You are {age} years old")
+print(type(age))
+
+
+
+
+
