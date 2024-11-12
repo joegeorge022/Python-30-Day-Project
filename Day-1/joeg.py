e04a3630c2f1bd40575fa796bd34b2850f9f95f0
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-11-12 20:39:24 +0530

    Create joeg.py

---
+++ Day-1/joeg.py
@@ -0,0 +1,12 @@
+full_name = "Joe George"
+age = "18"
+is_student = True
+ 
+print(f"Hello {full_name}!")
+print(f"Your name is {full_name} and you are {age} years old.")
+print(f"Are you a student? Ans: {is_student}")
+
+if is_student:
+    print("He is a student")
+else:
+    print("He is not a student") 
