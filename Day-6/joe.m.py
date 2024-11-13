242d73c7210182a87f04664ee208d72eca5361be
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2024-11-13 07:01:22 +0530

    Add files via upload

---
+++ Day-6/joe.m.py
@@ -0,0 +1,11 @@
+"""
+AUTHOR : JOE MARTIN RINCE
+DATE : 13-11-2024
+"""
+fruits=["apple","mango",'banana']
+print("Second fruit: ",fruits[1])
+fruits.append("orange")
+print("Fruits after adding orange : ",fruits)
+fruits.remove("banana")
+print("Fruits after removing banana:" ,fruits)
+print("Number of fruits : ",len(fruits))
