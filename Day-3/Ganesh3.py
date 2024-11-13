fd2144cc8c902b611836fd04a5b8e8bdf7b0b8cc
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2024-11-13 21:42:53 +0530

    Add files via upload

---
+++ Day-3/Ganesh3.py
@@ -0,0 +1,12 @@
+'''AUTHOR:GANESH
+13/11/24
+INPUT FUNCTION
+'''
+str=input("enter a sentence: ")
+number=int(input("enter a number: "))
+decimal=float(input("enter a decimal number: "))
+
+
+print("String:",str)
+print("Number:",number)
+print("Float:",decimal)
