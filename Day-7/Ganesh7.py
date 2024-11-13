e44be20302ed5ecad09d72e4e00279066b633c57
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2024-11-13 22:49:01 +0530

    Add files via upload

---
+++ Day-7/Ganesh7.py
@@ -0,0 +1,11 @@
+'''AUTHOR:GANESH
+13/11/24
+List Functions'''
+
+colours=["red","orange","blue","green","yellow","black"]
+for color in colours:
+    print("Color:",color)
+
+
+for index,color in enumerate(colours):
+    print(f"The positional index of {color} is {index}")
