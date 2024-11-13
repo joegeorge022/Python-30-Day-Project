6a8c7b27009ff620abce3d1ca72a03876400860f
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-11-13 22:53:17 +0530

    Create joe_g.py

---
+++ Day-6/joe_g.py
@@ -0,0 +1,14 @@
+''''
+Author: Joe George
+Date: 13 November 2024
+'''
+
+guys = ['Franklin','Michael','Trevor']
+
+print(guys[0])         # Print list guys
+
+guys.append("Jimmy")   # Add a new guy to the list
+
+guys.remove("Jimmy")   # Remove a guy from list
+
+print(len(guys))       # Prints list length
