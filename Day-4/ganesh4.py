394d84e90f0579149842f52c91d227cd5800cde5
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2024-11-13 22:07:05 +0530

    Add files via upload

---
+++ Day-4/ganesh4.py
@@ -0,0 +1,27 @@
+'''AUTHOR:GANESH
+13/11/24
+STRING FUNCTIONS'''
+name="Ganesh Chandran"
+age="19 Years old"
+
+
+full=name+" "+age
+print(full)
+
+statement="I Love Python"
+
+msg=statement[5:11]
+print(msg)
+
+
+main="Python is GOATED "
+
+
+print(main.lower())  
+print(main.upper())  
+print(main.split())   
+print(main.replace("GOATED", "great"))
+
+fr="Python is the \"Actual GOAT\""
+
+print(fr)
