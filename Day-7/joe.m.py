1b955adcf4afe491b89df0236b43236ed423bff8
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2024-11-14 07:00:01 +0530

    Add files via upload

---
+++ Day-7/joe.m.py
@@ -0,0 +1,10 @@
+"""
+AUTHOR : JOE MARTIN RINCE
+DATE : 13-11-2024
+"""
+friends=["dan","sam","joe","meg"]
+for friend in friends:
+    print(friend)
+
+for index , friends in enumerate(friends):
+    print(index,friends)
