0ddd05428922855d871261e0caf83ceeca7403d5
Jobthomas10 <jobthomascherian2028@cs.sjcetpalai.ac.in>
2024-11-14 10:30:33 -0800

    Add files via upload

---
+++ Day-7/job7.py
@@ -0,0 +1,10 @@
+"""Author :Job Thomas Cherian
+Date:14/11/24"""
+cars= ["bmw","mercedes","audi"]
+print(cars)
+for x in cars:
+    print(f"cars: {x}",)
+for i, cars in enumerate(cars):
+    print(i,cars)
+
+
