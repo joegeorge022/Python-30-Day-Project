2e1d93d48a66f19499644687c6022efa6ec4b05c
Jobthomas10 <jobthomascherian2028@cs.sjcetpalai.ac.in>
2024-11-20 10:26:20 -0800

    Add files via upload

---
+++ Day-10/job10.py
@@ -0,0 +1,15 @@
+numbers=[1,2,3,4]
+productOfNumbers=1
+max_number=numbers[0]
+min_number=numbers[0]
+for number in numbers :
+    productOfNumbers *= number
+    if number >max_number:
+        max_number=number
+    if number<min_number:
+        min_number=number
+average = productOfNumbers/len(numbers)
+print(productOfNumbers)
+print(min(numbers))
+print(max_number)
+print(average)
