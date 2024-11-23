942649bca0bb5be4aff98222515eb344d2d6c4e2
IndhuSubash-2007 <indhusubash2028@cs.sjcetpalai.ac.in>
2024-11-23 13:09:30 +0530

    Add files via upload

---
+++ Day-7/indhusubash7.py
@@ -0,0 +1,11 @@
+'''
+AUTHOR:INDHU SUBASH
+DATE:23-11-24
+'''
+
+games=["valorant","bgmi","assasincreed"]
+print(games)
+for j in games:
+    print(f"games:{j}",)
+for i, games in enumerate(games):
+    print(i,games)
