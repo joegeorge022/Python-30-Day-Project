8314164413fbaf0c5ad7b8f3377819d047062251
IndhuSubash-2007 <indhusubash2028@cs.sjcetpalai.ac.in>
2024-11-23 12:37:30 +0530

    Add files via upload

---
+++ Day-10/indhusubash.py
@@ -0,0 +1,19 @@
+'''
+AUTHOR:INDHU SUBASH
+DATE:23-11-24
+'''
+numbers=[1,2,3,4,5]
+productOfNumbers=1
+max_number=numbers[0]
+min_number=numbers[0]
+for number in numbers :
+    productOfNumbers *= number
+    if number >max_number:
+        max_number=number
+    if number<min_number:
+        min_number=number
+average = productOfNumbers/len(numbers)
+print(productOfNumbers)
+print(min(numbers))
+print(max_number)
+print(average)
