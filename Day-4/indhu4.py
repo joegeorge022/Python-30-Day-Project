d3687b348e592d792ff36f805f1224251f1f0f7d
IndhuSubash-2007 <indhusubash2028@cs.sjcetpalai.ac.in>
2024-11-23 13:45:45 +0530

    Add files via upload

---
+++ Day-4/indhu4.py
@@ -0,0 +1,16 @@
+greetings ="Hey"
+name =("ANU")
+
+full_greetings=greetings + "," + name + "!" +"  " + "Nice to meet you"
+print(full_greetings)
+
+message = "HI I AM ANU"
+print(message[0:2])
+
+sentence = "Python is awesome"
+print(sentence.upper())
+print(sentence.split())
+print(sentence.replace("awesome","great"))
+
+quote= "He said ,\"python is awesome.\""
+print(quote)
