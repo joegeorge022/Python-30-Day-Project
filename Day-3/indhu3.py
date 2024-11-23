5263891aa6258b301f5766d302a198c73a49f59a
IndhuSubash-2007 <indhusubash2028@cs.sjcetpalai.ac.in>
2024-11-23 13:24:16 +0530

    Add files via upload

---
+++ Day-3/indhu3.py
@@ -0,0 +1,12 @@
+'''
+Author: Indhu subash
+06/11/24
+'''
+
+input("Enter your name : ")
+
+int(input("Enter a number: "))
+
+float(input("Enter a decimal number : "))
+
+str(input("Do you love computer science? : "))
