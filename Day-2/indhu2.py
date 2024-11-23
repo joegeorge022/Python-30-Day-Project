ee6ee671bde19463c8350ed86ac687e90204a792
IndhuSubash-2007 <indhusubash2028@cs.sjcetpalai.ac.in>
2024-11-23 13:26:57 +0530

    Add files via upload

---
+++ Day-2/indhu2.py
@@ -0,0 +1,19 @@
+'''
+Author: Indhu Subash
+06/11/24
+
+'''
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+sum = a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+floor =a//b
+exponentional=a**b
+print(f"sum:{sum}")
+print(f"difference:{subtraction}")
+print(f"multiplication:{multiplication}")
+print(f"divison:{division}")
+print(f"floor:{floor}")
+print(f"exponentional:{exponentional}")
