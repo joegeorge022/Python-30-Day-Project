ae1e02c587fd11e39e10fb6f6c03e024cd441f9d
IndhuSubash-2007 <indhusubash2028@cs.sjcetpalai.ac.in>
2024-11-23 13:12:55 +0530

    Add files via upload

---
+++ Day-6/indhu6.py
@@ -0,0 +1,12 @@
+'''
+AUTHOR :INDHU SUBASH
+DATE : 23-11-2024
+'''
+
+fruits=["apple","mango","banana","kiwi"]
+print("Second fruit: ",fruits[1])
+fruits.append("grapes")
+print("Fruits after adding grapes : ",fruits)
+fruits.remove("banana")
+print("Fruits after removing banana:" ,fruits)
+print("Number of fruits : ",len(fruits))
