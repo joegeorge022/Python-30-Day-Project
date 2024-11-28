d0f6b0c2ade2f2c381842f6f1e029cf87665222b
Jobthomas10 <jobthomascherian2028@cs.sjcetpalai.ac.in>
2024-11-28 07:41:33 -0800

    Add files via upload

---
+++ Day-13/job.py
@@ -0,0 +1,10 @@
+"""
+Author:Job Thomas cherian
+Date:28/11/24
+"""
+my_info = ("job", "alappuzha", 18.5)
+print(my_info)
+print(my_info[1])
+print(my_info[2])
+name, place, age = my_info
+print("Name:",name,"\nplace:", place,"\nAge:" ,age)
