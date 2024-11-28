1b685d4984640eb7371c05eb56af595cdb20fd57
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2024-11-28 21:01:08 +0530

    Add files via upload

---
+++ Day-13/joe.m.py
@@ -0,0 +1,18 @@
+"""
+AUTHOR : JOE MARTIN RINCE
+DATE : 25-11-2024
+"""
+
+
+my_tuple = (1, "Python", 3.14, True, [1, 2, 3])
+print(my_tuple)
+
+my_tuple = (10, "Python", 3.14, "Hello", False)
+
+
+print(my_tuple[1])
+print(my_tuple[3])
+my_tuple = (1, "hi", 9.9)
+
+a, b, c = my_tuple
+print(a, b, c)
