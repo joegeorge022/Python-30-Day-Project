9218e6a21aaa00885461c7d956da4b7f75ce11f8
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2024-11-28 20:26:45 +0530

    Add files via upload

---
+++ Day-13/Ganesh13.py
@@ -0,0 +1,13 @@
+'''AUTHOR:GANESH
+28/11/24'''
+
+
+my_tup=(34,90,87)
+print("Original Tuple",my_tup)
+
+print("First element",my_tup[0])
+print("Second element",my_tup[1])
+print("Third element",my_tup[2])
+
+r,s,t=my_tup
+print("Unpacked tuples",r,s,t)
