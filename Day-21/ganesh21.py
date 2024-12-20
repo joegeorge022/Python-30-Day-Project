f8ae15bd0042fa0ac314becadd2845a5cefd2dfc
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2024-12-20 20:36:19 +0530

    Add files via upload

---
+++ Day-21/ganesh21.py
@@ -0,0 +1,26 @@
+
+
+strg="Hello World"
+print("Length of string",len(strg))
+      
+print("LOWER CASE",strg.lower())
+print("UPPER CASE:",strg.upper()) 
+
+
+
+print("Splitted",strg.split( ))
+
+print("is there any digits",strg.isdigit())
+print("Find world:",strg.find("world"))
+print()
+print()
+
+n=5
+g=1.8765
+l=[2,-6,4,6,7]
+print("absolute of",n,abs(n))
+print("max vaue",max(l))
+print("minimum value",min(l))
+print("sum",sum(l))
+print("round of",g,round(g))
+print("raised",n,"^2",pow(n,2))
