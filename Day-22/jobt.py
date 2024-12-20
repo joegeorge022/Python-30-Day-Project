bbb0592b99d319cd8bc791976017479f47058a4c
Jobthomas10 <jobthomascherian2028@cs.sjcetpalai.ac.in>
2024-12-20 23:28:40 +0530

    Update Day-22

---
+++ Day-22/jobt.py
@@ -0,0 +1,9 @@
+details= {'name':'Alice', 
+                'age':25,'city' :'New York'}
+print("details:" , details)
+#print("values:"details.values())
+print("keys:", details.keys())
+print("pairs",details.items(),end=" ") 
+pairs = list(details.items())
+
+print (pairs)
