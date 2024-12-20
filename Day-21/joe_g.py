d73726519353affb4d20be47f1779944cf14ec73
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-12-20 20:34:32 +0530

    Create joe_g.py

---
+++ Day-21/joe_g.py
@@ -0,0 +1,38 @@
+# STRING OPERTIONS
+string = "Hello, Joe!"
+
+print(len(string))
+
+print(string.lower())
+
+print(string.upper())
+
+print(string.replace("Hello,","Hola,"))
+
+print(string.find("J"))
+
+print(string.split(","))
+
+print(string.isdigit())
+
+
+# INTEGER OPERATIONS
+print(abs(-5))     #prints the absolute value or magnitude of the number.
+
+
+integer = [1,2,3,4,5]
+
+print(max(integer))
+
+print(min(integer))
+
+print(sum(integer))
+
+
+
+a = 1.234
+print(round(a))    # rounds of the number
+
+b = 2
+print(pow(b,10))   # prints 2 raised to 10
+
