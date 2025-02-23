0e6d82ad9689ffabdb12c4b214cda006351c3b6d
joegeorge022 <127773439+joegeorge022@users.noreply.github.com>
2025-02-23 14:09:38 +0530

    removed unnecessary lighting

--- js/main.js
+++ js/main.js
@@ -57,10 +57,3 @@ function initializeBackToTop() {
     window.addEventListener('scroll', toggleBackToTop);
     backToTop.addEventListener('click', scrollToTop);
 }
-
-document.addEventListener('mousemove', (e) => {
-    const x = (e.clientX / window.innerWidth) * 100;
-    const y = (e.clientY / window.innerHeight) * 100;
-    document.documentElement.style.setProperty('--x', x + '%');
-    document.documentElement.style.setProperty('--y', y + '%');
-});
